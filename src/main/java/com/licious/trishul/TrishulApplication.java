bb0712a5ab1a9a72a9533aaa55ce0cd30461298e
Trivendra Singh <trivendrasingh@licious.com>
2022-05-22 10:01:16 +0530

    first commit

---
+++ src/main/java/com/licious/trishul/TrishulApplication.java
@@ -0,0 +1,14 @@
+package com.licious.trishul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrishulApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Running application");
+		SpringApplication.run(TrishulApplication.class, args);
+	}
+
+}
